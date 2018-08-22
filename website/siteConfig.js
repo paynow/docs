5b2d35ad6d0ca87a4063efdcb7791f79ea1677ce
spaghettiwews <spaghettiwews@outlook.com>
2018-08-22 09:07:16 +0200

    added hero background to homepage

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -94,7 +94,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
-    theme: 'Pojoaque',
+    theme: 'Solarized Dark',
   },
 
   // Add custom scripts here that would be placed in <script> tags

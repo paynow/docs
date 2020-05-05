3d533039d6572dfc2fcebf80b214e7a75433406e
Webdev Group <44477596+webdevprojects-zw@users.noreply.github.com>
2020-05-05 09:34:39 +0200

    Delete runbuildlocally.sh

--- .circleci/runbuildlocally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=512b6edcb26926f7c1d4b48f442457c12c5c5108\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/paynow/docs/tree/master
